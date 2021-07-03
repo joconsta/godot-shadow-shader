73888243785f145c5b6a5fea45c5652138963864
joconsta <64503067+joconsta@users.noreply.github.com>
2021-07-03 13:12:39 -0700

    Handle dynamic sizing.

--- demo/rect_shadow.gd
+++ demo/rect_shadow.gd
@@ -1,11 +1,31 @@
+tool
 extends ColorRect
 
-export var corner_radius : int = 0
-export var shadow_size : int = 20
+export(int, 1, 20) var corner_radius = 0 setget set_corner_radius, get_corner_radius
+export(int, 1, 20) var shadow_size = 2 setget set_shadow_size, get_shadow_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-    material.set_shader_param("rect_position", rect_position)
-    material.set_shader_param("rect_end", rect_position + rect_size)
-    material.set_shader_param("rect_corner_radius", corner_radius)
-    material.set_shader_param("shadow_size", shadow_size)
+	# warning-ignore:return_value_discarded
+	connect("item_rect_changed", self, "_on_item_rect_changed")
+	material.set_shader_param("rect_position", rect_position)
+	material.set_shader_param("rect_end", rect_position + rect_size)
+	material.set_shader_param("rect_corner_radius", corner_radius)
+	material.set_shader_param("shadow_size", shadow_size)
+
+func set_corner_radius(value):
+	corner_radius = value
+	material.set_shader_param("rect_corner_radius", corner_radius)
+
+func get_corner_radius():
+	return corner_radius
+
+func set_shadow_size(value):
+	shadow_size = value
+	material.set_shader_param("shadow_size", shadow_size)
+
+func get_shadow_size():
+	return shadow_size
+
+func _on_item_rect_changed():
+	material.set_shader_param("rect_position", rect_position)
+	material.set_shader_param("rect_end", rect_position + rect_size)
